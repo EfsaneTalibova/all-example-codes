623f2cd258f3a4ab01f6fc8276c1f413997b0991
EfsaneTalibova <100290275+EfsaneTalibova@users.noreply.github.com>
2023-05-02 11:38:20 +0400

    react tours project

--- react-projects/react-tours/src/App.jsx
+++ react-projects/react-tours/src/App.jsx
@@ -7,6 +7,10 @@ const url = "https://course-api.com/react-tours-project";
 const App = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [tours, setTours] = useState([]);
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour) => tour.id !== id);
+    setTours(newTours);
+  };
   const fertchTours = async () => {
     setIsLoading(true);
     try {
@@ -28,6 +32,25 @@ const App = () => {
       </main>
     );
   }
-  return <main><Tours tours={tours}/></main>;
+  if (tours.length === 0) {
+    return (
+      <main>
+        <div className="title">
+          <h2>no tours left</h2>
+          <button
+            type="button"
+            style={{ marginTop: "2rem" }}
+            className="btn"
+            onClick={() => fertchTours()}
+          >refresh</button>
+        </div>
+      </main>
+    );
+  }
+  return (
+    <main>
+      <Tours tours={tours} removeTour={removeTour} />
+    </main>
+  );
 };
 export default App;
