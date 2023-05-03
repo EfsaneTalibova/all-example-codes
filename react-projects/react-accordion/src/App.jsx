a51b6c1d0fa1e1f4a2c7c90843be0d61a265e040
EfsaneTalibova <100290275+EfsaneTalibova@users.noreply.github.com>
2023-05-03 13:56:31 +0400

    butona klik etdikdə title görsənir yenidən click etdikdə görsənmir

--- react-projects/react-accordion/src/App.jsx
+++ react-projects/react-accordion/src/App.jsx
@@ -1,4 +1,21 @@
+import { useState } from "react";
+import Questions from "./Questions";
+import data from "./data";
 const App = () => {
-  return <h2>Accordion Starter</h2>;
+  const [questions, setQuestions] = useState(data);
+  const [activeId, setActiveId] = useState(null);
+  const toggleQuestion = (id) => {
+    const newActiveId=id===activeId?null:id
+    setActiveId(newActiveId)
+  };
+  return (
+    <main>
+      <Questions
+        questions={questions}
+        activeId={activeId}
+        toggleQuestion={toggleQuestion}
+      />
+    </main>
+  );
 };
 export default App;
---
+++ react-projects/react-accordion/src/SingleQuestion.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
+const SingleQuestion = ({ id, title, info, activeId, toggleQuestion }) => {
+  const isActive = id === activeId; //isActive return true or false value
+  return (
+    <article className="question">
+      <header>
+        <h5>{title}</h5>
+        <button onClick={() => toggleQuestion(id)} className="question-btn">
+          {isActive ? <AiOutlineMinus /> : <AiOutlinePlus />}
+        </button>
+      </header>
+      {isActive && <p>{info}</p>}
+    </article>
+  );
+};
+
+export default SingleQuestion;
