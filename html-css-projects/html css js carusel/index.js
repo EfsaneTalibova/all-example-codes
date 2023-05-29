da4a6c2e8a5504f570a9aff32087739a5587c859
EfsaneTalibova <100290275+EfsaneTalibova@users.noreply.github.com>
2023-05-29 16:18:49 +0400

    carusel html css js

---
+++ html-css-projects/html css js carusel/index.js
@@ -0,0 +1,26 @@
+const carousel = document.querySelector(".carousel");
+
+let isDragStart = false ,prevPageX, prevScrollLeft;
+const dragStart = (e) => {
+  isDragStart = true;
+  prevPageX=e.pageX;
+  prevScrollLeft = carousel.scrollLeft
+  console.log(e.pageX);
+  console.log( carousel.scrollLeft);
+};
+
+const dragging = (e) => {
+  //    console.log(e.pageX);
+  if (!isDragStart) return;
+  e.preventDefault();
+  let positionDiff = e.pageX-prevPageX;
+  carousel.scrollLeft = prevScrollLeft-positionDiff;
+};
+
+const dragStop = () => {
+  isDragStart = false;
+};
+
+carousel.addEventListener("mousedown", dragStart);
+carousel.addEventListener("mousemove", dragging);
+carousel.addEventListener("mouseup", dragStop);
