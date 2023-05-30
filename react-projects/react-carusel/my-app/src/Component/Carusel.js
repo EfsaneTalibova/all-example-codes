8a8d8e5a730fd0f7686a2e50918b62c9c510c57f
EfsaneTalibova <100290275+EfsaneTalibova@users.noreply.github.com>
2023-05-30 12:45:17 +0400

    oxlarla olan hissə də hazırdır

--- react-projects/react-carusel/my-app/src/Component/Carusel.js
+++ react-projects/react-carusel/my-app/src/Component/Carusel.js
@@ -14,8 +14,8 @@ const Carusel = () => {
     setIsDragStart(true);
     setPrevPageX(e.pageX);
     setPrevScrollLeft(carouselRef.current.scrollLeft);
-    console.log(e.pageX);
-    console.log(carouselRef.current.scrollLeft);
+    // console.log(e.pageX);
+    // console.log(carouselRef.current.scrollLeft);
   };
 
   const dragging = (e) => {
@@ -29,31 +29,38 @@ const Carusel = () => {
   const dragStop = () => {
     setIsDragStart(false);
   };
-  useEffect(() => {
-    const carouselElement = carouselRef.current;
-    carouselElement.addEventListener('mousedown', dragStart);
-    carouselElement.addEventListener('mousemove', dragging);
-    carouselElement.addEventListener('mouseup', dragStop);
+  // useEffect(() => {
+  //   const carouselElement = carouselRef.current;
+  //   carouselElement.addEventListener('mousedown', dragStart);
+  //   carouselElement.addEventListener('mousemove', dragging);
+  //   carouselElement.addEventListener('mouseup', dragStop);
 
-    return () => {
-      carouselElement.removeEventListener('mousedown', dragStart);
-      carouselElement.removeEventListener('mousemove', dragging);
-      carouselElement.removeEventListener('mouseup', dragStop);
-    };
-  }, [dragStart, dragging, dragStop]);
+  //   return () => {
+  //     carouselElement.removeEventListener('mousedown', dragStart);
+  //     carouselElement.removeEventListener('mousemove', dragging);
+  //     carouselElement.removeEventListener('mouseup', dragStop);
+  //   };
+  // }, [dragStart, dragging, dragStop]);
+  const handleArrowClick = (event) => {
+    const carousel = carouselRef.current;
+    const iconId = event.target.id;
+    const firstImg = carousel.querySelectorAll("img")[0];
+    const firstImgWidth = firstImg.clientWidth + 14;
+    carousel.scrollLeft += iconId === "left" ? -firstImgWidth : firstImgWidth;
+  };
   return (
     <div className="carouselContainer">
       <div className="carouselContainer_wrapper">
-        <FontAwesomeIcon className="carousel_icon" icon={faAngleLeft} />
-        <div  ref={carouselRef} className="carousel_elements">
+        <FontAwesomeIcon id="left" className="carousel_icon" icon={faAngleLeft} onClick={handleArrowClick}/>
+        <div  ref={carouselRef} onMouseDown={dragStart} onMouseMove={dragging} onMouseUp={dragStop} className="carousel_elements">
           <img src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg" alt="" />
+          <img src="https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aHVtYW58ZW58MHx8MHx8fDA%3D&w=1000&q=80" alt="" />
+          <img src="https://images.pexels.com/photos/674010/pexels-photo-674010.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="" />
           <img src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg" alt="" />
-          <img src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg" alt="" />
-          <img src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg" alt="" />
-          <img src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg" alt="" />
-          <img src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg" alt="" />
+          <img src="https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aHVtYW58ZW58MHx8MHx8fDA%3D&w=1000&q=80" alt="" />
+          <img src="https://images.pexels.com/photos/674010/pexels-photo-674010.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="" />
         </div>
-        <FontAwesomeIcon className="carousel_icon" icon={faAngleRight} />
+        <FontAwesomeIcon id="right"  className="carousel_icon" icon={faAngleRight} onClick={handleArrowClick}/>
       </div>
     </div>
   );
