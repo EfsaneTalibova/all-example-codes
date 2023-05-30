a8e8ca237822d1580b644258f3ef3e5002580885
EfsaneTalibova <100290275+EfsaneTalibova@users.noreply.github.com>
2023-05-30 12:29:04 +0400

    scrol oxlarla da işləyir

--- html-css-projects/html css js carusel/index.js
+++ html-css-projects/html css js carusel/index.js
@@ -1,20 +1,22 @@
 const carousel = document.querySelector(".carousel");
 
-let isDragStart = false ,prevPageX, prevScrollLeft;
+let isDragStart = false,
+  prevPageX,
+  prevScrollLeft;
 const dragStart = (e) => {
   isDragStart = true;
-  prevPageX=e.pageX;
-  prevScrollLeft = carousel.scrollLeft
-  console.log(e.pageX);
-  console.log( carousel.scrollLeft);
+  prevPageX = e.pageX;
+  prevScrollLeft = carousel.scrollLeft;
+  // console.log(e.pageX);
+  // console.log(carousel.scrollLeft);
 };
 
 const dragging = (e) => {
   //    console.log(e.pageX);
   if (!isDragStart) return;
   e.preventDefault();
-  let positionDiff = e.pageX-prevPageX;
-  carousel.scrollLeft = prevScrollLeft-positionDiff;
+  let positionDiff = e.pageX - prevPageX;
+  carousel.scrollLeft = prevScrollLeft - positionDiff;
 };
 
 const dragStop = () => {
@@ -24,3 +26,15 @@ const dragStop = () => {
 carousel.addEventListener("mousedown", dragStart);
 carousel.addEventListener("mousemove", dragging);
 carousel.addEventListener("mouseup", dragStop);
+
+// oxlarla keçidlər
+
+arrowIcons = document.querySelectorAll(".wrapper i ");
+firstImg = carousel.querySelectorAll("img")[0];
+let firstImgWidth = firstImg.clientWidth + 14; 
+
+arrowIcons.forEach((icon) => {
+  icon.addEventListener("click", () => {
+    carousel.scrollLeft += icon.id == "left" ? -firstImgWidth : firstImgWidth;
+  });
+});
